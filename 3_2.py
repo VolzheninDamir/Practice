ea17877f83083f8d2bae3e1d415104af8deb6bb1
VolzheninDamir <volzhenin.damir2004@yandex.ru>
2023-07-01 15:25:59 +0700

    Pt_3_2: Full task

---
+++ 3_2.py
@@ -0,0 +1,5 @@
+fibonacci = lambda n: [0, 1] if n == 1 else fibonacci(n-1) + [fibonacci(n-1)[-1] + fibonacci(n-1)[-2]] if n > 1 else []
+
+n = int(input("Введите число: "))
+fibonacci_numbers = fibonacci(n)
+print("Список чисел Фибоначчи до", n, ":", fibonacci_numbers)
