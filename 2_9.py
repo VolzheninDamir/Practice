3a0aeab199cc1432203d6f44aadfe98c42d4d596
VolzheninDamir <volzhenin.damir2004@yandex.ru>
2023-07-01 15:25:09 +0700

    Pt_2_9: Full task

---
+++ 2_9.py
@@ -0,0 +1,12 @@
+import collections
+number = int(input("Введите число"))
+const_number = number
+nums_reverse = []
+while number > 0:
+    num = number % 10
+    nums_reverse.append(num)
+    number = number // 10
+nums = nums_reverse[::-1]
+mx = max(nums)
+index = nums.index(mx)
+print(f'Порядковый номер максимальной цифры {mx} в числе {const_number} от начала числа = {index + 1}, а от конца = {len(nums) - index}')
