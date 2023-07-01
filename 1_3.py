1d719eecb6a0c5432d29891fdb6130dc061e8af0
VolzheninDamir <volzhenin.damir2004@yandex.ru>
2023-07-01 15:22:30 +0700

    Pt_1_3: Full task

---
+++ 1_3.py
@@ -0,0 +1,18 @@
+nums  = []
+
+one = int(input("Введите первое число: "))
+nums.append(one)
+two = int(input("Введите второе число: "))
+nums.append(two)
+three = int(input("Введите третье число: "))
+nums.append(three)
+
+if (one > two) and (one > three):
+    print("Наибольшее число: ", one)
+elif (two > one) and (two > three):
+    print("Наибольшее число: ", two)
+elif (one == two == three):
+    print("Наибольшего нет, числа равны")
+else: print("Наибольшее число: ", three)
+r_nums = list(reversed(sorted(nums)))
+print(r_nums)
