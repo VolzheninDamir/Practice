380c40c9ab467b94cbc3daf9d12e7f4638d6df69
VolzheninDamir <volzhenin.damir2004@yandex.ru>
2023-07-01 15:23:10 +0700

    Pt_1_9: Full task

---
+++ 1_9.py
@@ -0,0 +1,5 @@
+a = int(input("Введите первое число - "))
+b = int(input("Введите второе число - "))
+composition = a * b
+string = f'Произведение чисел {a} и {b} равно {composition}.'
+print(string)
