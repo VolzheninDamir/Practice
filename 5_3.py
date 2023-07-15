e1dd9925f3bcde5a79d9f560fec186c2cde890ad
VolzheninDamir <volzhenin.damir2004@yandex.ru>
2023-07-15 12:37:14 +0700

    Pt_5_3: Full task, corrected for PEP 8

---
+++ 5_3.py
@@ -0,0 +1,17 @@
+import csv
+
+with open("books.csv", encoding='utf-8') as books:
+    file_reader = csv.DictReader(books, delimiter=",")
+    first = input("Введите начальный год: ")
+    last = input("Введите конечный год: ")
+    found = False
+    for row in file_reader:
+        try:
+            year = int(row["Год выпуска"])
+            if int(first) <= year <= int(last):
+                found = True
+                print(row["Книга"], row["Год выпуска"], sep=", ")
+        except ValueError:
+            continue
+    if not found:
+        print("В списке нет книг в интервале этих дат.")
