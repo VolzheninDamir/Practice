996e31fc359f7b52871aef7da81a0d4f9ff1c57d
VolzheninDamir <volzhenin.damir2004@yandex.ru>
2023-07-17 15:03:13 +0700

    Pt_2_8: Full task, corrected for PEP 8

--- 2_8.py
+++ 2_8.py
@@ -1,7 +1,7 @@
 n = int(input("Введите число"))
 if n >= 0:
-    for i in range(0, n+2):
-        if i**2 > n:
+    for i in range(0, n + 2):
+        if i ** 2 > n:
             print(i)
             break
         else:
