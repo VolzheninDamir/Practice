fab6a3e7d53639c9bd53aabc2a00c0a32a703e80
volzhenindamir <volzhenin.damir2004@yandex.ru>
2023-07-20 18:52:51 +0700

    Pt_3_8: Full task, corrected PEP8

--- 3_8.py
+++ 3_8.py
@@ -2,6 +2,6 @@ alpha_dict = {}
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
 for i in range(len(alphabet)):
-    alpha_dict[alphabet[i]] = i+1
+    alpha_dict[alphabet[i]] = i + 1
 
 print(alpha_dict)
