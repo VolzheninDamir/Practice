0f1bf02580ac8cabfb335887759349d1e43e4f18
volzhenindamir <volzhenin.damir2004@yandex.ru>
2023-07-20 18:52:05 +0700

    Pt_3_2: Full task, corrected PEP8

--- 3_2.py
+++ 3_2.py
@@ -1,4 +1,5 @@
-fibonacci = lambda n: [0, 1] if n == 1 else fibonacci(n-1) + [fibonacci(n-1)[-1] + fibonacci(n-1)[-2]] if n > 1 else []
+fibonacci = lambda n: [0, 1] if n == 1 else fibonacci(n - 1) + [
+    fibonacci(n - 1)[-1] + fibonacci(n - 1)[-2]] if n > 1 else []
 
 n = int(input("Введите число: "))
 fibonacci_numbers = fibonacci(n)
